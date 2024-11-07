5479e0a72d8a4c189865c9c5dec60eb30b39a1fd
quikrish <reach.me@keemail.me>
2024-11-07 23:07:07 +0530

    restructured createGrid function.Add event handler to reset button
    made changes in css
    change in background image of the page

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
- // Select the grid container
-const container = document.querySelector('.container');
+// Set up variables to retrieve HTML and CSS elements 
+const container = document.querySelector('.grid-container');
 
-// Function to create a 16x16 grid
-function createGrid(rows,columns){
-    const totalCells = rows * columns;
-    for(let i = 1; i <= totalCells; i++){
-        // Create a new div element for each cell
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        // Append the cell to the grid container
-        container.appendChild(cell);
-    }
+const reset = document.getElementById('reset');
+const defaultColor = document.getElementById('default');
+const randomColor = document.getElementById('color');
+
+const resolutionInput = document.querySelector('#resolutionInput');
+const resolutionLabel = document.querySelector('.resolutionLabel');
+let gridSelection = 8;
+
+resolutionInput.addEventListener('input',changeGrid);
+
+function changeGrid(e){
+
+    gridSelection = e.target.value;
+    resolutionLabel.textContent = `${gridSelection}x${gridSelection}`;
 }
 
-// createGrid(16, 16);
+// Creates grid div boxes on load
+createGrid(gridSelection);
+
+// Function to create a grid
+function createGrid(size){
+    let totalCells = size * size;
+    for(let i = 0; i < totalCells; ++i){
+        // Create a new div element for each cell
+        const gridDiv = document.createElement('div');
+        gridDiv.classList.add('box');
+        // Append the cell to the grid container
+
+        // gridDiv.addEventListener('mouseover',changeColor);
+        container.appendChild(gridDiv);
+    }
+
+    container.style.gridTemplateRows = `repeat(${size},1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size},1fr)`;
+}
+
+// Added event listener for our reset button
+reset.addEventListener('click',resetGrid);
+
+function resetGrid(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    createGrid(gridSelection);
+}
