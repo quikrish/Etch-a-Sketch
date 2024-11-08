cdc890f9bd7279a01b70beac2db252d8d3e415dd
quikrish <reach.me@keemail.me>
2024-11-08 19:21:13 +0530

    changes in script file create event handler for buttons reset, default and color create function to change color when mouseover the grid

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const randomColor = document.getElementById('color');
 const resolutionInput = document.querySelector('#resolutionInput');
 const resolutionLabel = document.querySelector('.resolutionLabel');
 let gridSelection = 8;
+let type;
 
 resolutionInput.addEventListener('input',changeGrid);
 
@@ -23,13 +24,13 @@ createGrid(gridSelection);
 // Function to create a grid
 function createGrid(size){
     let totalCells = size * size;
-    for(let i = 0; i < totalCells; ++i){
+    for(let i = 0; i < totalCells; i++){
         // Create a new div element for each cell
         const gridDiv = document.createElement('div');
         gridDiv.classList.add('box');
         // Append the cell to the grid container
 
-        // gridDiv.addEventListener('mouseover',changeColor);
+        gridDiv.addEventListener('mouseover',changeColor);
         container.appendChild(gridDiv);
     }
 
@@ -46,3 +47,27 @@ function resetGrid(){
     }
     createGrid(gridSelection);
 }
+
+function changeColor(e){
+    if(type === "randomColorMode"){
+        const randomR = Math.floor(Math.random()*256);
+        const randomG = Math.floor(Math.random()*256);
+        const randomB = Math.floor(Math.random()*256);
+        e.target.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+    }
+    else if(type === "defaultColorMode"){
+        e.target.style.backgroundColor = '#ffffff';
+    }
+}
+
+defaultColor.addEventListener('click',function(){
+    type = "defaultColorMode";
+    this.classList.add('active');
+    randomColor.classList.remove('active');
+});
+
+randomColor.addEventListener('click',function(){
+    type = "randomColorMode";
+    this.classList.add('active');
+    defaultColor.classList.remove('active');
+});
